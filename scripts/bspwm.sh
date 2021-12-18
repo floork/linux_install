ed8f49456a44dfd2fcdcbc21f0c3f4f4a92b7333
Florian Mros <48620727+floork@users.noreply.github.com>
2021-12-18 14:01:21 +0100

    nice

---
+++ scripts/bspwm.sh
@@ -0,0 +1,11 @@
+#install bspwm
+sudo pacman -S bspwm
+
+#configure
+mkdir -p ~/.config/bspwm
+mkdir -p ~/.config/sxhkd
+wget -O ~/.config/bspwm/bspwmrc https://raw.githubusercontent.com/ChrisTitusTech/BSPWM-on-KDE/main/bspwmrc
+wget -O ~/.config/sxhkd/sxhkdrc https://raw.githubusercontent.com/ChrisTitusTech/BSPWM-on-KDE/main/sxhkdrc
+chmod +x ~/.config/bspwm/bspwmrc
+chmod +x ~/.config/sxhkd/sxhkdrc
+sudo wget -O /usr/share/xsessions/plasma-bspwm.desktop https://raw.githubusercontent.com/ChrisTitusTech/BSPWM-on-KDE/main/plasma-bspwm.desktop
