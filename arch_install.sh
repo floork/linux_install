4b748533d84b2212107b11a4999b784833fb67c9
Florian Mros <48620727+floork@users.noreply.github.com>
2021-12-23 11:44:15 +0100

    better

--- arch_install.sh
+++ arch_install.sh
@@ -1,7 +1,9 @@
 cd ./scripts
-./install_fish.sh
+./install_yay.sh
 ./install_flatpak_und_programms.sh
 ./install_snap_snapstore_programms.sh
+./install_fish.sh
 ./install_virtual_box.sh
-./install_yay.sh
 ./pacman_programm_list.sh
+./fixlocale.sh
+./arch_keyboard.sh
--- scripts/install_yay.sh
+++ scripts/install_yay.sh
@@ -3,9 +3,20 @@ sudo pacman -Syu
 sudo pacman -S git
 cd /opt
 sudo git clone https://aur.archlinux.org/yay-git.git
-sudo chown -R tecmint:tecmint ./yay-git
+sudo chown -R floork:floork ./yay-git
 cd yay-git
 makepkg -si
 
 #install programms
-yay -S brave celluloid code-git dropbox github-desktop-bin github-cli pycharm-community-edition spotify timeshift htop nmon
+yay -S brave
+yay -S celluloid
+yay -S code-git
+yay -S dropbox
+yay -S github-desktop-bin
+yay -S github-cli
+yay -S pycharm-community-edition
+yay -S spotify
+yay -S timeshift
+yay -S htop
+yay -S nmon
+yay -S alacritty
