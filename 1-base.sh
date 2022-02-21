5230d64c4e9b927925721299be3ae509599162e9
Florian Mros <48620727+floork@users.noreply.github.com>
2022-02-21 14:08:14 +0100

    passt

--- 1-base.sh
+++ 1-base.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
 
+logo () {
+# This will be shown on every set as user is progressing
 echo -ne "
 
 ███╗   ███╗██╗   ██╗██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗
@@ -13,3 +14,58 @@ echo -ne "
                 Welcome to my Post-Install Script
 --------------------------------------------------------------------------
 "
+}
+timezone () {
+# Added this from arch wiki https://wiki.archlinux.org/title/System_time
+time_zone="$(curl --fail https://ipapi.co/timezone)"
+echo -ne "System detected your timezone to be '$time_zone' \n"
+echo -ne "Is this correct? yes/no:"
+read answer
+case $answer in
+    y|Y|yes|Yes|YES)
+    set_option TIMEZONE $time_zone;;
+    n|N|no|NO|No)
+    echo "Please enter your desired timezone e.g. Europe/London :"
+    read new_timezone
+    set_option TIMEZONE $new_timezone;;
+    *) echo "Wrong option. Try again";timezone;;
+esac
+}
+keymap () {
+# These are default key maps as presented in official arch repo archinstall
+echo -ne "
+Please select key board layout from this list
+    -by
+    -ca
+    -cf
+    -cz
+    -de
+    -dk
+    -es
+    -et
+    -fa
+    -fi
+    -fr
+    -gr
+    -hu
+    -il
+    -it
+    -lt
+    -lv
+    -mk
+    -nl
+    -no
+    -pl
+    -ro
+    -ru
+    -sg
+    -ua
+    -uk
+    -us
+"
+read -p "Your key boards layout:" keymap
+set_option KEYMAP $keymap
+}
+
+
+
--- config/theming.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#set theme in home
-sudo cp -R home/plasma /home/$USER/.local/share/plasma/
-sudo cp -R home/candy-icons /home/$USER/.local/share/icons/
-sudo cp -R home/config/* /home/$USER/.config
-
-#set theme in usr
-sudo cp -R usr/* /usr/share/icons
