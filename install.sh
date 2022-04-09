6322ecf452ab82144e5d85db46c01320c359fa02
floork <florian.mros@mail.de>
2022-04-09 16:16:15 +0200

    fixed a bug

--- install.sh
+++ install.sh
@@ -18,20 +18,22 @@ logo () {
 }
 
 numlock(){
-    echo -ne "
+        echo -ne "
         Do you want to use Numlockx?
+        "
+        echo -ne "
         1) Yes
         0) No
-    Choose an option: "
-    read -r numlockx
-    case ${numlockx} in
+        Choose an option: "
+        read -r numlockx
+        case ${numlockx} in
         1)
             echo -ne "
         -------------------------------------------------------------------------
                                     Numlockx
         -------------------------------------------------------------------------
             "
-            yay -S numlockx
+            yay -S  --noconfirm --needed numlockx
             echo "numlockx on" | sudo tee /etc/X11/xinit/xinitrc
             echo "[General]" | sudo tee /etc/sddm.conf
             echo "Numlock=on" | sudo tee -a /etc/sddm.conf
@@ -42,17 +44,17 @@ numlock(){
             echo "Please only use 1 or 0"
             numlock
         ;;
-    esac
+        esac
 }
 
 cursor(){
-    echo -ne "
+        echo -ne "
         Do you want to use the future-black-cursor?
-    "
-    echo -ne"
-1) Yes
-0) No
-Choose an option: "
+        "
+        echo -ne"
+        1) Yes
+        0) No
+        Choose an option: "
         read -r answer
         case ${answer} in
         1)
@@ -70,54 +72,54 @@ Choose an option: "
     }
 
 installtype() {
-echo -ne "
-Do you want to install all the Programms?
-1) Yes
-0) No
-    Choose an option:  "
-            read -r install_type
-            case install_type in
-            1)
-                cat ${SCRIPT_DIR}/pkgs/pacman-pkgs.txt | while read line
-                do
-                    echo "INSTALLING: ${line}"
-                    sudo pacman -S --noconfirm --needed ${line}
-                done
-
-                cat ${SCRIPT_DIR}/pkgs/aur-pkgs.txt | while read line
-                do
-                    echo "INSTALLING Yay-Packages: ${line}"
-                    yay -S --noconfirm --needed ${line}
-                done
-
-                flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-                cat ${SCRIPT_DIR}/pkgs/flatpaks.txt | while read line
-                do
-                    echo "INSTALLING Flatpak's: ${line}"
-                   flatpak install -y --noninteractive flathub ${line}
-                done
-                #give flatpak access to themes
-                sudo flatpak override --filesystem=~/.themes
-            ;;
-            0)
-            ;;
-            *)
-                echo "Please only use 1 or 0"
-                installtype
-            ;;
-            esac
+        echo -ne "
+        Do you want to install all the Programms?
+        "
+        echo -ne "
+        1) Yes
+        0) No
+        Choose an option:  "
+        read -r install_type
+        case $(install_type) in
+        1)
+            cat ${SCRIPT_DIR}/pkgs/pacman-pkgs.txt | while read line
+            do
+                echo "INSTALLING: ${line}"
+                sudo pacman -S --noconfirm --needed ${line}
+            done
+         cat ${SCRIPT_DIR}/pkgs/aur-pkgs.txt | while read line
+            do
+                echo "INSTALLING Yay-Packages: ${line}"
+                yay -S --noconfirm --needed ${line}
+            done
+         flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+            cat ${SCRIPT_DIR}/pkgs/flatpaks.txt | while read line
+            do
+                echo "INSTALLING Flatpak's: ${line}"
+               flatpak install -y --noninteractive flathub ${line}
+            done
+            #give flatpak access to themes
+            sudo flatpak override --filesystem=~/.themes
+        ;;
+        0)
+        ;;
+        *)
+            echo "Please only use 1 or 0"
+            installtype
+        ;;
+        esac
     }
 
 konsa(){
-    echo -ne "
-    Do you want to install all KDE Configs?"
-    echo -ne "
-            1) ALL
-            2) Only the shortcuts
-            0) Do nothing
-    Choose an option:  "
-    read -r kon
-    case ${kon} in
+        echo -ne "
+        Do you want to install all KDE Configs?"
+        echo -ne "
+        1) ALL
+        2) Only the shortcuts
+        0) Do nothing
+        Choose an option:  "
+        read -r kon
+        case ${kon} in
         1)
             cp -r ${SCRIPT_DIR}/configs/.config/* ~/.config/
             pip install konsave
@@ -134,45 +136,45 @@ konsa(){
             echo "Please only use 1 or 0"
             konsa
         ;;
-    esac
-}
+        esac
+    }
 
 shell(){
-    curl -sS https://starship.rs/install.sh | sh
-    git clone https://github.com/floork/my_shell
-    cd my_shell
-    bash "install.sh"
-    cd $SCRIPT_DIR
-}
+        curl -sS https://starship.rs/install.sh | sh
+        git clone https://github.com/floork/my_shell
+        cd my_shell
+        bash "install.sh"
+        cd $SCRIPT_DIR
+    }
 
 logo2(){
-    echo -ne "
+        echo -ne "
 
-        ███╗   ███╗██╗   ██╗██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗
-        ████╗ ████║╚██╗ ██╔╝██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║
-        ██╔████╔██║ ╚████╔╝ ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║
-        ██║╚██╔╝██║  ╚██╔╝  ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║
-        ██║ ╚═╝ ██║   ██║   ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗
-        ╚═╝     ╚═╝   ╚═╝   ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝
-        -------------------------------------------------------------------------
+            ███╗   ███╗██╗   ██╗██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗
+            ████╗ ████║╚██╗ ██╔╝██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║
+            ██╔████╔██║ ╚████╔╝ ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║
+            ██║╚██╔╝██║  ╚██╔╝  ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║
+            ██║ ╚═╝ ██║   ██║   ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗
+            ╚═╝     ╚═╝   ╚═╝   ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝
+            -------------------------------------------------------------------------
 
-                                        Finished!!
+                                            Finished!!
 
-        -------------------------------------------------------------------------
-    "
-}
+            -------------------------------------------------------------------------
+        "
+    }
 
 del(){
-    echo -ne "
-    Do you want to delete this script?"
-    echo -ne "
-            1) Yes, delete and reboot
-            2) Only delete
-            3) Only reboot
-            0) No
-    Choose an option:  "
-    read -r delt
-    case ${delt} in
+        echo -ne "
+        Do you want to delete this script?"
+        echo -ne "
+        1) Yes, delete and reboot
+        2) Only delete
+        3) Only reboot
+        0) No
+        Choose an option:  "
+        read -r delt
+        case ${delt} in
         1)
             rm -r ${SCRIPT_DIR}
             echo "Your System will Reboot in 5 seconds"
@@ -193,14 +195,14 @@ del(){
             echo "Please only use 1 or 0"
             del
         ;;
-    esac
-}
+        esac
+    }
 
 rebot (){
-    echo "Your System will Reboot in 5 seconds"
-    sleep 5s
-    reboot -h now
-}
+        echo "Your System will Reboot in 5 seconds"
+        sleep 5s
+        reboot -h now
+    }
 
 logo
 numlock
