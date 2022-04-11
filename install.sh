a529bff3b67bca723bad198cfae10291a8830a10
floork <mros.florian@gmail.com>
2022-04-11 14:57:39 +0200

    added fedora

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-SCRIPT_DIR="$HOME/linux_install"
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export SCRIPT_DIR
 
 logo () {
     echo -ne "
@@ -17,98 +19,31 @@ logo () {
     "
 }
 
-numlock(){
-        echo -ne "
-        Do you want to use Numlockx?
-        "
-        echo -ne "
-        1) Yes
-        0) No
-        Choose an option: "
-        read -r numlockx
-        case ${numlockx} in
-        1)
-            echo -ne "
-        -------------------------------------------------------------------------
-                                    Numlockx
-        -------------------------------------------------------------------------
-            "
-            yay -S  --noconfirm --needed numlockx
-            echo "numlockx on" | sudo tee /etc/X11/xinit/xinitrc
-            echo "[General]" | sudo tee /etc/sddm.conf
-            echo "Numlock=on" | sudo tee -a /etc/sddm.conf
+sys (){
+    echo -ne "
+        What Distro do you use?"
+    echo -ne "
+        1) Arch
+        2) Fedora
+        0) Ubuntu/Debian
+        Choose an option:   "
+        read -r distro
+        case ${distro} in
+        1) 
+            bash ${SCRIPT_DIR}/scripts/arch.sh
+        ;;
+        2)
+            bash ${SCRIPT_DIR}/scripts/fedora.sh
         ;;
         0)
+            bash ${SCRIPT_DIR}/scripts/debian.sh
         ;;
         *)
             echo "Please only use 1 or 0"
-            numlock
+            sys
         ;;
         esac
 }
-
-cursor(){
-        echo -ne "
-        Do you want to use the future-black-cursor?
-        "
-        echo -ne "
-        1) Yes
-        0) No
-        Choose an option: "
-        read -r answer
-        case ${answer} in
-        1)
-        sudo cp -r ${SCRIPT_DIR}/configs/Future-black-cursors /usr/share/icons
-        echo '[Icon Theme]' | sudo tee /usr/share/icons/default/index.theme
-        echo 'Inherits=Future-black Cursors' | sudo tee -a /usr/share/icons/default/index.theme
-        ;;
-        0)
-        ;;
-        *)
-        echo "Please only use 1 or 0"
-        cursor
-        ;;
-        esac
-    }
-
-installtype() {
-        echo -ne "
-        Do you want to install all the Programms?
-        "
-        echo -ne "
-        1) Yes
-        0) No
-        Choose an option:  "
-        read -r install_type
-        case ${install_type} in
-        1)
-            cat ${SCRIPT_DIR}/pkgs/pacman-pkgs.txt | while read line
-            do
-                echo "INSTALLING: ${line}"
-                sudo pacman -S --noconfirm --needed ${line}
-            done
-         cat ${SCRIPT_DIR}/pkgs/aur-pkgs.txt | while read line
-            do
-                echo "INSTALLING Yay-Packages: ${line}"
-                yay -S --noconfirm --needed ${line}
-            done
-         flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-            cat ${SCRIPT_DIR}/pkgs/flatpaks.txt | while read line
-            do
-                echo "INSTALLING Flatpak's: ${line}"
-               flatpak install -y --noninteractive flathub ${line}
-            done
-            #give flatpak access to themes
-            sudo flatpak override --filesystem=~/.themes
-        ;;
-        0)
-        ;;
-        *)
-            echo "Please only use 1 or 0"
-            installtype
-        ;;
-        esac
-    }
 
 konsa(){
         echo -ne "
@@ -199,11 +134,8 @@ del(){
         esac
     }
 
-
 logo
-numlock
-cursor
-installtype
+sys
 konsa
 shell
 logo2
---
+++ scripts/fedora_rep.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#rpmfusion
+sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# VS Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf check-update
+
+# Github Desktop
+sudo rpm --import https://packagecloud.io/shiftkey/desktop/gpgkey
+sudo sh -c 'echo -e "[shiftkey]\nname=GitHub Desktop\nbaseurl=https://packagecloud.io/shiftkey/desktop/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/shiftkey/desktop/gpgkey" > /etc/yum.repos.d/shiftkey-desktop.repo'
+
+# Brave
+sudo dnf install dnf-plugins-core
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
