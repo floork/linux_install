7175f82b72d407f007ba64c60ed79cd71f237732
floork <mros.florian@gmail.com>
2022-05-13 14:15:33 +0200

    auto detect which package manager

--- install.sh
+++ install.sh
@@ -19,41 +19,27 @@ logo () {
 }
 
 sys (){
-    echo -ne "
-        What Distro do you use?"
-    echo -ne "
-        1) Arch
-        2) Fedora
-        0) Ubuntu/Debian
-        Choose an option:   "
-        read -r distro
-        case ${distro} in
-        1) 
-            bash ${SCRIPT_DIR}/scripts/arch.sh
-        ;;
-        2)
-            bash ${SCRIPT_DIR}/scripts/fedora.sh
-        ;;
-        0)
-            bash ${SCRIPT_DIR}/scripts/debian.sh
-        ;;
-        *)
-            echo "Please only use 1 or 0"
-            sys
-        ;;
-        esac
+    if [ -x "$(command -v pacman)" ];then
+        bash ${SCRIPT_DIR}/scripts/arch.sh
+        elif [ -x "$(command -v dnf)" ];then
+        bash ${SCRIPT_DIR}/scripts/fedora.sh
+        elif [ -x "$(command -v apt-get)" ];then
+        bash ${SCRIPT_DIR}/scripts/debian.sh
+    else
+        echo 'This Distro is not supported!'
+    fi
 }
 
 konsa(){
-        echo -ne "
-        Do you want to install all KDE Configs?"
-        echo -ne "
+    echo -ne "
+    Do you want to install all KDE Configs?"
+    echo -ne "
         1) ALL
         2) Only the shortcuts
         0) Do nothing
-        Choose an option:  "
-        read -r kon
-        case ${kon} in
+    Choose an option:  "
+    read -r kon
+    case ${kon} in
         1)
             sudo cp -r ${SCRIPT_DIR}/configs/.config/* ~/.config/
             sudo cp ${SCRIPT_DIR}/configs/index.theme /usr/share/icons/default/
@@ -72,20 +58,20 @@ konsa(){
             echo "Please only use 1 or 0"
             konsa
         ;;
-        esac
-    }
+    esac
+}
 
 shell(){
-        curl -sS https://starship.rs/install.sh | sh
-        git clone https://github.com/floork/my_shell
-        cd my_shell
-        bash "install.sh"
-        cd $SCRIPT_DIR
-        clear
-    }
+    curl -sS https://starship.rs/install.sh | sh
+    git clone https://github.com/floork/my_shell
+    cd my_shell
+    bash "install.sh"
+    cd $SCRIPT_DIR
+    clear
+}
 
 logo2(){
-        echo -ne "
+    echo -ne "
 
             ███╗   ███╗██╗   ██╗██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗
             ████╗ ████║╚██╗ ██╔╝██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║
@@ -98,20 +84,20 @@ logo2(){
                                             Finished!!
 
             -------------------------------------------------------------------------
-        "
-    }
+    "
+}
 
 del(){
-        echo -ne "
-        Do you want to delete this script?"
-        echo -ne "
+    echo -ne "
+    Do you want to delete this script?"
+    echo -ne "
         1) Yes, delete and reboot
         2) Only delete
         3) Only reboot
         0) No
-        Choose an option:  "
-        read -r delt
-        case ${delt} in
+    Choose an option:  "
+    read -r delt
+    case ${delt} in
         1)
             rm -r ${SCRIPT_DIR}
             echo "Your System will Reboot in 5 seconds"
@@ -132,8 +118,8 @@ del(){
             echo "Please only use 1 or 0"
             del
         ;;
-        esac
-    }
+    esac
+}
 
 export SCRIPT_DIR
 
